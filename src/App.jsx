de44d56759cfd46cb866af618f5129f1c8a101c0
ObiWanWheeler <charliestinhorto@gmail.com>
2021-02-20 21:56:22 +0000

    add state

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,39 @@
 import React from 'react';
+
 import CardList from './CardList.jsx';
 import { robots } from "./robots";
+
+import SearchBox from './SearchBox.jsx';
+
+import "./App.css"
+import "tachyons";
 
 
 class App extends React.Component {
 
+    constructor() {
+        super();
+        this.state = {
+            robots: robots,
+            searchField: '',
+        }
+    }
+
+    OnSearchChange = (event) => {
+        this.setState({ searchField: event.target.value });
+    }
+
     render() {
+        const filteredBots = robots.filter(
+            robot => {
+                return robot.name.toLowerCase().includes(this.state.searchField.trim().toLowerCase());
+            });
         return (
-            <React.Fragment>
-                <h1>RobotFriends</h1>
-                
-                <CardList robots={robots}/>
-            </React.Fragment>
+            <div className="tc">
+                <h1 className="f1">RobotFriends</h1>
+                <SearchBox searchChange={this.OnSearchChange} />
+                <CardList robots={filteredBots} requestedRobots={this.state.searchField}/>
+            </div>
         )
     }
 }
