dae734feda3bd663761d39338253631143f73132
ObiWanWheeler <charliestinhorto@gmail.com>
2021-02-20 22:27:36 +0000

    add api call

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,38 @@
 import React from 'react';
 
 import CardList from './CardList.jsx';
-import { robots } from "./robots";
 
 import SearchBox from './SearchBox.jsx';
 
 import "./App.css"
 import "tachyons";
 
+const robot_endpoint = "https://jsonplaceholder.typicode.com/users";
 
 class App extends React.Component {
 
     constructor() {
         super();
         this.state = {
-            robots: robots,
+            robots: [],
             searchField: '',
         }
     }
 
-    OnSearchChange = (event) => {
-        this.setState({ searchField: event.target.value });
+    componentDidMount() {
+        fetch(robot_endpoint)
+        .then(response => response.json())
+        .then(users => this.setState( {robots: users }));
     }
 
     render() {
-        const filteredBots = robots.filter(
-            robot => {
-                return robot.name.toLowerCase().includes(this.state.searchField.trim().toLowerCase());
-            });
+
+        if (this.state.robots.length === 0) {
+            return <h1>Loading...</h1>
+        }
+
+        const filteredBots = this.filterRobots();
+
         return (
             <div className="tc">
                 <h1 className="f1">RobotFriends</h1>
@@ -36,6 +41,17 @@ class App extends React.Component {
             </div>
         )
     }
+
+    filterRobots() {
+        return this.state.robots.filter(
+            robot => {
+                return robot.name.toLowerCase().includes(this.state.searchField.trim().toLowerCase());
+            });
+    }
+
+    OnSearchChange = (event) => {
+        this.setState({ searchField: event.target.value });
+    };
 }
 
 export default App;
